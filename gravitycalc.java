352cdab5773196a19fccdfa3b0f90ce17fcdc833
ankur210105 <150512452+ankur210105@users.noreply.github.com>
2024-01-10 23:22:26 +0530

    Gravity Calculator
    easy simple gravity calculator at your service

---
+++ gravitycalc.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class gravitycalc
+{
+    //function that calculates the position of falling object
+    /*x(f) = 0.5 * at^2 + v(i) + x(i)
+    a = acceleration = -9.81 m/s
+    t = time (in seconds) = 10
+    v(i) = initial velocity
+    x(i) = initial position*/
+    static double position()
+    {
+        Scanner obj=new Scanner(System.in);
+        System.out.println("Enter the acceleration");
+        double a=obj.nextDouble();
+        System.out.println("Enter the time( in seconds )");
+        int t=obj.nextInt();
+        System.out.println("Enter the initial velocity");
+        double v=obj.nextDouble();
+        System.out.println("Enter the initial position");
+        double p=obj.nextDouble();
+        return 0.5*(a*t*t)+(v*t)+p; //returns the final position
+    }
+    //main invoking
+    public static void main(String[] args) 
+    {
+        double pos=position();
+        System.out.println("The final position of the body is"+pos);    
+        //final position of the body is printed
+    }
+}
---
+++ prob.java
@@ -0,0 +1,16 @@
+class hello
+{
+    public static void main(String[] args) 
+    {
+            String s1 = "hello";
+            String s2 = new String("hello");
+            if(s1==s2)
+            {
+                System.out.println("a"); 
+            }
+            else
+            {
+                System.out.println("b");
+            }
+    }         
+}
